f454b43e8909ccd3d2c4ccbbe43ccf0e46669000
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-09-02 10:36:39 +0100

    seed file with 8 apartments

--- backend/src/resources/user/router.js
+++ backend/src/resources/user/router.js
@@ -8,12 +8,21 @@ const {
 } = require('./controller')
 const { createOneApartment } = require('../apartment/controller')
 
+//USER
+userRouter.post('/', createOneUser)
 userRouter.get('/:id', getOneUser)
 userRouter.get('/:id/info', getOneUserPlusInfo)
+//***patch needs to be created
 
+//USERS
 userRouter.get('/', getAllUsers)
 
-userRouter.post('/', createOneUser)
+//CREATE ONE USER'S APARTMENT
 userRouter.post('/:id/apartments', createOneApartment)
+//***patch needs to be created
+
+//GET ONE USER'S APARTMENT
+
+//GET ALL USER'S APARTMENTS
 
 module.exports = userRouter
