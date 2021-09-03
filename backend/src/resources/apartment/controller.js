a1beecdc6e018b47a6b3834c11feeb67f85607a6
Anas Ansari <anas_ansar1@outlook.com>
2021-09-03 17:20:37 +0100

    created getApartments by city name

--- backend/src/resources/apartment/controller.js
+++ backend/src/resources/apartment/controller.js
@@ -1,78 +1,78 @@
 //include (get something from the same model)
 //select (connects models with relationship)
 //connect (on create/update if you have to connect a model to another)
-const nodeFetch = require('node-fetch')
-const { apartment } = require('../../utils/dbClient')
+const nodeFetch = require("node-fetch");
+const { apartment } = require("../../utils/dbClient");
 
 //CREATE ONE USER'S APARTMENT
 async function createOneApartment(req, res) {
-	const userOwnerId = req.params.id
-	console.log('userOwnerId: ', userOwnerId)
-	const {
-		priceNight,
-		bedrooms,
-		maxPeopleIn,
-		description,
-		city,
-		postCode,
-		road,
-		imageUrl1,
-		imageUrl2,
-		imageUrl3,
-		wifi,
-		smartTV,
-		microwave,
-		coffeeMaker,
-		hotTub,
-		parkingSpace,
-		garden,
-		pool,
-		gym,
-	} = req.body
+  const userOwnerId = req.params.id;
+  console.log("userOwnerId: ", userOwnerId);
+  const {
+    priceNight,
+    bedrooms,
+    maxPeopleIn,
+    description,
+    city,
+    postCode,
+    road,
+    imageUrl1,
+    imageUrl2,
+    imageUrl3,
+    wifi,
+    smartTV,
+    microwave,
+    coffeeMaker,
+    hotTub,
+    parkingSpace,
+    garden,
+    pool,
+    gym,
+  } = req.body;
 
-	const newApartment = {
-		priceNight,
-		bedrooms,
-		maxPeopleIn,
-		description,
-		city,
-		postCode,
-		road,
-		imageUrl1,
-		imageUrl2,
-		imageUrl3,
-		extra: {
-			create: {
-				wifi,
-				smartTV,
-				microwave,
-				coffeeMaker,
-				hotTub,
-				parkingSpace,
-				garden,
-				pool,
-				gym,
-			},
-		},
-		location: {
-			create: {
-				latitude,
-				longitude,
-			},
-		},
-	}
+  const newApartment = {
+    priceNight,
+    bedrooms,
+    maxPeopleIn,
+    description,
+    city,
+    postCode,
+    road,
+    imageUrl1,
+    imageUrl2,
+    imageUrl3,
+    extra: {
+      create: {
+        wifi,
+        smartTV,
+        microwave,
+        coffeeMaker,
+        hotTub,
+        parkingSpace,
+        garden,
+        pool,
+        gym,
+      },
+    },
+    location: {
+      create: {
+        latitude,
+        longitude,
+      },
+    },
+  };
 
-	const createdApartment = await apartment.create({
-		data: {
-			...newApartment,
-			userOwner: {
-				connect: {
-					id: parseInt(userOwnerId),
-				},
-			},
-		},
-	})
-	res.json({ data: createdApartment })
+  const createdApartment = await apartment.create({
+    data: {
+      ...newApartment,
+      userOwner: {
+        connect: {
+          id: parseInt(userOwnerId),
+        },
+      },
+    },
+  });
+  res.json({ data: createdApartment });
 }
 
 //GET ONE USER'S APARTMENT
@@ -87,4 +87,22 @@ async function createOneApartment(req, res) {
 // 	res.json({ data: oneApartment })
 // }
 
-module.exports = { createOneApartment }
+async function getApartmentsByCity(req, res) {
+  const city = req.params.city;
+
+  try {
+    const result = await apartment.findMany({
+      where: { city: { equals: city, mode: "insensitive" } },
+    });
+    if (result) res.json(result);
+    if (!result) res.json({ msg: "City not found" });
+  } catch (e) {
+    console.log(e);
+    res.json(e.message);
+  }
+}
+
+module.exports = {
+  createOneApartment,
+  getApartmentsByCity,
+};
--- backend/src/resources/user/router.js
+++ backend/src/resources/user/router.js
@@ -1,35 +1,39 @@
-const userRouter = require('express').Router()
+const userRouter = require("express").Router();
 
 const {
-	createOneUser,
-	getOneUser,
-	getAllUsers,
-	getOneUserPlusInfo,
-	getAllApartments,
-	// updateOneUser,
-} = require('./controller')
+  createOneUser,
+  getOneUser,
+  getAllUsers,
+  getOneUserPlusInfo,
+  getAllApartments,
+  // updateOneUser,
+} = require("./controller");
 const {
-	createOneApartment,
-	// updateOneApartment
-} = require('../apartment/controller')
+  createOneApartment,
+  getApartmentsByCity,
+  // updateOneApartment
+} = require("../apartment/controller");
 
 //USER
-userRouter.post('/', createOneUser)
-userRouter.get('/:id', getOneUser)
-userRouter.get('/:id/info', getOneUserPlusInfo)
+userRouter.post("/", createOneUser);
+userRouter.get("/:id", getOneUser);
+userRouter.get("/:id/info", getOneUserPlusInfo);
 // userRouter.patch('/:id', updateOneUser)
 
 //USERS
-userRouter.get('/', getAllUsers)
+userRouter.get("/", getAllUsers);
 
 //CREATE ONE USER'S APARTMENT
-userRouter.post('/:id/apartments', createOneApartment)
+userRouter.post("/:id/apartments", createOneApartment);
 // userRouter.patch('/:id/apartments/:id', updateOneApartment)
 
 //GET ONE USER'S APARTMENT
-userRouter.get('/:id/apartments', getAllApartments)
+userRouter.get("/:id/apartments", getAllApartments);
 // userRouter.get('/:id/apartments/:id', getOneApartment)
 
 //GET ALL USER'S APARTMENTS
 
-module.exports = userRouter
+//GET ALL APARTMENTS BY CITY
+userRouter.get("/apartments/:city", getApartmentsByCity);
+
+module.exports = userRouter;
