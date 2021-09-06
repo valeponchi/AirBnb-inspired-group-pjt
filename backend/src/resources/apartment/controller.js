8c1678378a2d81623117d942fd1ab2e692269518
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-09-06 19:37:39 +0100

    lat-long

--- backend/src/resources/apartment/controller.js
+++ backend/src/resources/apartment/controller.js
@@ -2,91 +2,91 @@
 //select (connects models with relationship)
 //connect (on create/update if you have to connect a model to another)
 
-const { apartment, apartmentLocation } = require("../../utils/dbClient");
+const { apartment, apartmentLocation } = require('../../utils/dbClient')
 
 const fetch = require('node-fetch')
-// import fetch from 'node-fetch'
-// const fetch = (...args) =>
-// 	import('node-fetch').then(({ default: fetch }) => fetch(...args))
-// https://geocode.xyz/${address}?json=1&auth=434249204703844806348x95363
-// api.postcodes.io/postcodes/RH194SA
+
 const address = 'RH19 4SA'
 const response = fetch(`https://api.postcodes.io/postcodes/${address}`)
 	.then(resp => resp.json())
-	.then(data => console.log(data.result.longitude))
+	.then(data =>
+		console.log(
+			`longitude: ${data.result.longitude}, latitude: ${data.result.latitude}`
+		)
+	)
 // console.log('address longitude: ', response.result.longitude)
 // const data = await response.json()
 // console.log(data)
 
 //CREATE ONE USER'S APARTMENT
 async function createOneApartment(req, res) {
-  const userOwnerId = req.params.id;
-  console.log("userOwnerId: ", userOwnerId);
-  const {
-    priceNight,
-    bedrooms,
-    maxPeopleIn,
-    description,
-    city,
-    postCode,
-    road,
-    imageUrl1,
-    imageUrl2,
-    imageUrl3,
-    wifi,
-    smartTV,
-    microwave,
-    coffeeMaker,
-    hotTub,
-    parkingSpace,
-    garden,
-    pool,
-    gym,
-  } = req.body;
+	const userOwnerId = req.params.id
+	console.log('userOwnerId: ', userOwnerId)
+	const {
+		priceNight,
+		bedrooms,
+		maxPeopleIn,
+		description,
+		city,
+		postCode,
+		road,
+		imageUrl1,
+		imageUrl2,
+		imageUrl3,
+		wifi,
+		smartTV,
+		microwave,
+		coffeeMaker,
+		hotTub,
+		parkingSpace,
+		garden,
+		pool,
+		gym,
+	} = req.body
 
-  const newApartment = {
-    priceNight,
-    bedrooms,
-    maxPeopleIn,
-    description,
-    city,
-    postCode,
-    road,
-    imageUrl1,
-    imageUrl2,
-    imageUrl3,
-    extra: {
-      create: {
-        wifi,
-        smartTV,
-        microwave,
-        coffeeMaker,
-        hotTub,
-        parkingSpace,
-        garden,
-        pool,
-        gym,
-      },
-    },
-    location: {
-      create: {
-        latitude,
-        longitude,
-      },
-    },
-  };
+	const newApartment = {
+		priceNight,
+		bedrooms,
+		maxPeopleIn,
+		description,
+		city,
+		postCode,
+		road,
+		imageUrl1,
+		imageUrl2,
+		imageUrl3,
+		extra: {
+			create: {
+				wifi,
+				smartTV,
+				microwave,
+				coffeeMaker,
+				hotTub,
+				parkingSpace,
+				garden,
+				pool,
+				gym,
+			},
+		},
+		location: {
+			create: {
+				latitude,
+				longitude,
+			},
+		},
+	}
 
-  const createdApartment = await apartment.create({
-    data: {
-      ...newApartment,
-      userOwner: {
-        connect: {
-          id: parseInt(userOwnerId),
-        },
-      },
-    },
-  });
-  res.json({ data: createdApartment });
+	const createdApartment = await apartment.create({
+		data: {
+			...newApartment,
+			userOwner: {
+				connect: {
+					id: parseInt(userOwnerId),
+				},
+			},
+		},
+	})
+	res.json({ data: createdApartment })
 }
 
 //GET ONE USER'S APARTMENT
@@ -102,29 +102,29 @@ async function createOneApartment(req, res) {
 // }
 
 async function getApartmentsByCity(req, res) {
-  const searchLocation = req.params.city;
+	const searchLocation = req.params.city
 
-  try {
-    const result = await apartment.findMany({
-      where: {
-        city: {
-          equals: searchLocation,
-          mode: "insensitive",
-        },
-      },
-      include: {
-        location: true,
-      },
-    });
-    if (result) res.json(result);
-    if (!result) res.json({ msg: "City not found" });
-  } catch (e) {
-    console.log(e);
-    res.json(e.message);
-  }
+	try {
+		const result = await apartment.findMany({
+			where: {
+				city: {
+					equals: searchLocation,
+					mode: 'insensitive',
+				},
+			},
+			include: {
+				location: true,
+			},
+		})
+		if (result) res.json(result)
+		if (!result) res.json({ msg: 'City not found' })
+	} catch (e) {
+		console.log(e)
+		res.json(e.message)
+	}
 }
 
 module.exports = {
-  createOneApartment,
-  getApartmentsByCity,
-};
+	createOneApartment,
+	getApartmentsByCity,
+}
