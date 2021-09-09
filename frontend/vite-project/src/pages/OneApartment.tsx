6c8332e43850979131c2e0b5479b8a3415a34570
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-09-09 11:01:29 +0100

    click-apart-card and goes to its own page

--- frontend/vite-project/src/pages/OneApartment.tsx
+++ frontend/vite-project/src/pages/OneApartment.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from "react";
-import { useHistory } from "react-router";
-import useStore from "../store";
+import React, { useEffect } from 'react'
+import { useHistory } from 'react-router'
+import useStore from '../store'
 
 export default function OneApartment() {
-  const loggedUser = useStore(state => state.loggedUser);
-  const history = useHistory();
+	const loggedUser = useStore(state => state.loggedUser)
+	const history = useHistory()
 
-  useEffect(() => {
-    if (!loggedUser) history.push("/login-host");
-  }, [loggedUser]);
-  return null;
+	// useEffect(() => {
+	//   if (!loggedUser) history.push("/login-host");
+	// }, [loggedUser]);
+	return null
 }
