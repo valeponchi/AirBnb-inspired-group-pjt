349d5d27ea9d2b6ce4602a96dace842b7ead98c1
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-09-10 13:41:43 +0100

    hostAddPAge in progress

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -10,20 +10,17 @@ import OneApartmentHost from './pages/OneApartmentHost'
 
 import HostDashBoardPage from './pages/HostDashBoardPage'
 
-
 import LoginPage, { UserCredentials } from './pages/LoginPage'
 import HostingPage from './pages/HostingPage'
 import Footer from './components/Footer'
+import HostAddOne from './pages/HostAddOne'
 
 // type LoginProps = {
 // 	className: string
 // }
 
-
-
 function App() {
-
-	const [userLoggedIn, setUserLoggedIn] = useState(false);
+	const [userLoggedIn, setUserLoggedIn] = useState(false)
 	console.log(userLoggedIn)
 	return (
 		<div className="App ">
@@ -32,29 +29,51 @@ function App() {
 					<Redirect to="/home" />
 				</Route>
 				<Route path="/home">
-					<HomePage userLoggedIn={userLoggedIn} setUserLoggedIn={setUserLoggedIn} />
+					<HomePage
+						userLoggedIn={userLoggedIn}
+						setUserLoggedIn={setUserLoggedIn}
+					/>
 				</Route>
 				<Route path="/placestostay">
-					<PlacesToStayPage userLoggedIn={userLoggedIn} setUserLoggedIn={setUserLoggedIn} />
+					<PlacesToStayPage
+						userLoggedIn={userLoggedIn}
+						setUserLoggedIn={setUserLoggedIn}
+					/>
 				</Route>
 				<Route path="/hosting">
 					<HostingPage />
 				</Route>
-				<Route path="/addhostproperty"></Route>
+
 				{/* <Route path="/:acc"></Route>  This is causing a bug*/}
 				<Route path="/login-host">
 					<LoginPage setUserLoggedIn={setUserLoggedIn} />
 				</Route>
 				<Route path="/staysin/:search">
-					<StaysInAreaPage userLoggedIn={userLoggedIn} setUserLoggedIn={setUserLoggedIn} />
+					<StaysInAreaPage
+						userLoggedIn={userLoggedIn}
+						setUserLoggedIn={setUserLoggedIn}
+					/>
 				</Route>
 
 				<Route path="/apartment/:id/:apartmentId/:postCode">
-					<OneApartmentHost userLoggedIn={userLoggedIn} setUserLoggedIn={setUserLoggedIn}/>
+					<OneApartmentHost
+						userLoggedIn={userLoggedIn}
+						setUserLoggedIn={setUserLoggedIn}
+					/>
 				</Route>
 
 				<Route path="/dashboard/:id">
-					<HostDashBoardPage userLoggedIn={userLoggedIn} setUserLoggedIn={setUserLoggedIn} />
+					<HostDashBoardPage
+						userLoggedIn={userLoggedIn}
+						setUserLoggedIn={setUserLoggedIn}
+					/>
+				</Route>
+
+				<Route path="/addhostproperty">
+					<HostAddOne
+						userLoggedIn={userLoggedIn}
+						setUserLoggedIn={setUserLoggedIn}
+					/>
 				</Route>
 				{/* <Route path="/:accomidationname">
           <h1>Hello world</h1>
--- frontend/vite-project/src/pages/HostDashBoardPage.tsx
+++ frontend/vite-project/src/pages/HostDashBoardPage.tsx
@@ -1,233 +1,230 @@
-import React from "react";
-import styled from "styled-components";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import { BsPlusCircle } from "react-icons/bs";
-import { BiUpArrowAlt, BiCalendar } from "react-icons/bi";
-import PropertyCardImage from "../components/PropertyCardImage";
-import { Link, useHistory } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { useParams } from "react-router";
+import React from 'react'
+import styled from 'styled-components'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import { BsPlusCircle } from 'react-icons/bs'
+import { BiUpArrowAlt, BiCalendar } from 'react-icons/bi'
+import PropertyCardImage from '../components/PropertyCardImage'
+import { Link, useHistory } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router'
 
-import useStore from "../store";
+import useStore from '../store'
 
 function HostDashBoardPage({ className, userLoggedIn, setUserLoggedIn }) {
-  // Get current user ID
-  const [currentUserId, setCurrentUserId] = useState(1);
-  const [hostedProperties, setHostedProperties] = useState([]);
-  let amountOfProperties = 0;
-  //Fetch with user id
+	// Get current user ID
+	const [currentUserId, setCurrentUserId] = useState(1)
+	const [hostedProperties, setHostedProperties] = useState([])
+	let amountOfProperties = 0
+	//Fetch with user id
 
-  const params = useParams()
-  
+	const params = useParams()
 
+	useEffect(() => {
+		setCurrentUserId(params.id)
+		fetch(`http://localhost:4000/users/${currentUserId}/apartments`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			credentials: 'include',
+		})
+			.then(resp => resp.json())
+			.then(data => setHostedProperties([data]))
+	}, [])
 
-  useEffect(() => {
-    setCurrentUserId(params.id)
-    fetch(`http://localhost:4000/users/${currentUserId}/apartments`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    })
-      .then(resp => resp.json())
-      .then(data => setHostedProperties([data]));
-  }, []);
+	function loadingContent() {
+		if (hostedProperties.length === 1) {
+			const limitAparments = hostedProperties[0].data[0].apartmentOwned.slice(
+				0,
+				4
+			)
+			amountOfProperties = limitAparments.length
 
-  function loadingContent() {
-    if (hostedProperties.length === 1) {
-      const limitAparments = hostedProperties[0].data[0].apartmentOwned.slice(
-        0,
-        4
-      );
-      amountOfProperties = limitAparments.length;
+			return (
+				<>
+					{limitAparments.map(apartment => {
+						return (
+							<PropertyCardImage
+								Apartment={apartment}
+								currentUserId={currentUserId}
+								key={limitAparments.postCode}
+							/>
+						)
+					})}
+				</>
+			)
+		} else {
+			return <h1>Loading Properties</h1>
+		}
+	}
 
-      return (
-        <>
-          {limitAparments.map(apartment => {
-            return (
-              <PropertyCardImage
-                Apartment={apartment}
-                currentUserId={currentUserId}
-                key={limitAparments.postCode}
-              />
-            );
-          })}
-        </>
-      );
-    } else {
-      return <h1>Loading Properties</h1>;
+	return (
+		<div className={className}>
+			<Header userLoggedIn={userLoggedIn} setUserLoggedIn={setUserLoggedIn} />
+			<main className="container">
+				<ul>
+					<li>
+						<a href="#AddHostedProperty">Add Hosted Property</a>
+					</li>
+					<li>
+						<a href="#ReviewHostedProperty">Review Hosted Property</a>
+					</li>
+					<li>
+						<a href="#ViewBookings">View Bookings</a>
+					</li>
+				</ul>
 
-    }
-  }
-
-  return (
-    <div className={className}>
-      <Header userLoggedIn={userLoggedIn} setUserLoggedIn={setUserLoggedIn} />
-      <main className="container">
-        <ul>
-          <li>
-            <a href="#AddHostedProperty">Add Hosted Property</a>
-          </li>
-          <li>
-            <a href="#ReviewHostedProperty">Review Hosted Property</a>
-          </li>
-          <li>
-            <a href="#ViewBookings">View Bookings</a>
-          </li>
-        </ul>
-
-        <h3>Welcome To Host Dashboard</h3>
-        <section className="hostedProperty" id="AddHostedProperty">
-          <h3>Add Hosted Property</h3>
-          <div className="card">
-            <p>Click Add Button</p>
-            <Link to="/addhostproperty">
-              <BsPlusCircle className="plusIcon" />
-            </Link>
-          </div>
-        </section>
-        <section className="reviewHostedProperty" id="ReviewHostedProperty">
-          <h3>Review Hosted Properties</h3>
-          <div className="pictureCardContainer">{loadingContent()}</div>
-        </section>
-        <section id="ViewBookings" className="viewBookings">
-          <h3>View Bookings</h3>
-          <div className="bookingsCardsContainer">
-            <div className="bookingCard">
-              <BiUpArrowAlt className="icon" />
-              <p>+£1500</p>
-            </div>
-            <div className="bookingCard">
-              <BiCalendar className="icon" />
-              <p>Calender</p>
-            </div>
-            <div className="bookingCard">
-              <p>{amountOfProperties} Properties Hosted</p>
-            </div>
-          </div>
-        </section>
-      </main>
-      <Footer />
-    </div>
-  );
+				<h3>Welcome To Host Dashboard</h3>
+				<section className="hostedProperty" id="AddHostedProperty">
+					<h3>Add Hosted Property</h3>
+					<div className="card">
+						<p>Click Add Button</p>
+						<Link to="/addhostproperty">
+							<BsPlusCircle className="plusIcon" />
+						</Link>
+					</div>
+				</section>
+				<section className="reviewHostedProperty" id="ReviewHostedProperty">
+					<h3>Review Hosted Properties</h3>
+					<div className="pictureCardContainer">{loadingContent()}</div>
+				</section>
+				<section id="ViewBookings" className="viewBookings">
+					<h3>View Bookings</h3>
+					<div className="bookingsCardsContainer">
+						<div className="bookingCard">
+							<BiUpArrowAlt className="icon" />
+							<p>+£1500</p>
+						</div>
+						<div className="bookingCard">
+							<BiCalendar className="icon" />
+							<p>Calender</p>
+						</div>
+						<div className="bookingCard">
+							<p>{amountOfProperties} Properties Hosted</p>
+						</div>
+					</div>
+				</section>
+			</main>
+			<Footer />
+		</div>
+	)
 }
 
 export default styled(HostDashBoardPage)`
-  display: grid;
+	display: grid;
 
-  grid-gap: 30px;
+	grid-gap: 30px;
 
-  ul {
-    list-style-type: none;
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(100px, 250px));
-    place-content: center;
-  }
+	ul {
+		list-style-type: none;
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(100px, 250px));
+		place-content: center;
+	}
 
-  li {
-    display: inline-block;
-    place-self: center;
-  }
+	li {
+		display: inline-block;
+		place-self: center;
+	}
 
-  a {
-    text-decoration: none;
-    color: black;
-    text-align: center;
-  }
+	a {
+		text-decoration: none;
+		color: black;
+		text-align: center;
+	}
 
-  .pictureCardContainer {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 250px));
-    grid-gap: 20px;
-  }
+	.pictureCardContainer {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(200px, 250px));
+		grid-gap: 20px;
+	}
 
-  main {
-    display: grid;
-    grid-gap: 30px;
-  }
+	main {
+		display: grid;
+		grid-gap: 30px;
+	}
 
-  h3 {
-    place-self: center;
-    font-size: 1.5rem;
-  }
+	h3 {
+		place-self: center;
+		font-size: 1.5rem;
+	}
 
-  .card {
-    border: 1px solid lightgrey;
-    position: relative;
-    width: 300px;
-    height: 300px;
-    border-radius: 20px;
-    display: grid;
-    place-content: center;
-    place-items: center;
-  }
+	.card {
+		border: 1px solid lightgrey;
+		position: relative;
+		width: 300px;
+		height: 300px;
+		border-radius: 20px;
+		display: grid;
+		place-content: center;
+		place-items: center;
+	}
 
-  .card p {
-    font-size: 1.5rem;
-  }
+	.card p {
+		font-size: 1.5rem;
+	}
 
-  .plusIcon {
-    position: absolute;
-    top: -3%;
-    right: -3%;
-    font-size: 2rem;
-    background-color: white;
-    z-index: 10;
-  }
+	.plusIcon {
+		position: absolute;
+		top: -3%;
+		right: -3%;
+		font-size: 2rem;
+		background-color: white;
+		z-index: 10;
+	}
 
-  .plusIcon:hover {
-    fill: green;
-  }
+	.plusIcon:hover {
+		fill: green;
+	}
 
-  .reviewHostedProperty {
-    display: grid;
-    grid-gap: 20px;
-  }
+	.reviewHostedProperty {
+		display: grid;
+		grid-gap: 20px;
+	}
 
-  .reviewHostedProperty h3 {
-    place-self: start;
-  }
+	.reviewHostedProperty h3 {
+		place-self: start;
+	}
 
-  .hostedProperty {
-    display: grid;
-    grid-gap: 20px;
-  }
+	.hostedProperty {
+		display: grid;
+		grid-gap: 20px;
+	}
 
-  .hostedProperty h3 {
-    place-self: start;
-  }
+	.hostedProperty h3 {
+		place-self: start;
+	}
 
-  .bookingsCardsContainer {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
-  }
+	.bookingsCardsContainer {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+	}
 
-  .bookingCard {
-    width: 350px;
-    height: 200px;
-    border: 1px solid lightgrey;
-    display: grid;
-    place-content: center;
-    place-items: center;
-    border-radius: 20px;
-  }
+	.bookingCard {
+		width: 350px;
+		height: 200px;
+		border: 1px solid lightgrey;
+		display: grid;
+		place-content: center;
+		place-items: center;
+		border-radius: 20px;
+	}
 
-  .bookingCard .icon {
-    font-size: 4rem;
-  }
+	.bookingCard .icon {
+		font-size: 4rem;
+	}
 
-  .bookingCard p {
-    font-size: 1.5rem;
-  }
+	.bookingCard p {
+		font-size: 1.5rem;
+	}
 
-  .viewBookings {
-    display: grid;
-    grid-gap: 20px;
-  }
+	.viewBookings {
+		display: grid;
+		grid-gap: 20px;
+	}
 
-  .viewBookings h3 {
-    place-self: start;
-  }
-`;
+	.viewBookings h3 {
+		place-self: start;
+	}
+`
